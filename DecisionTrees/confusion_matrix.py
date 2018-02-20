e7f6236c671c2a6119d33ef8bbfc02b384608816
daravinds <daravinds@gmail.com>
2018-02-19 20:23:30 -0600

    Confusion Matrix Part 3

---
+++ DecisionTrees/confusion_matrix.py
@@ -0,0 +1,16 @@
+import sys
+import csv
+
+def build_confusion_matrix(file):
+  matrix = [[0 for x in range(3)] for y in range(3)] 
+  
+  with open(file, 'rb') as csvfile:
+    records = csv.reader(csvfile)
+    for record in records:
+      row = int(record[1]) - 1
+      col = int(record[0]) - 1
+      matrix[row][col] = matrix[row][col] + 1;
+  print matrix
+
+if __name__ == "__main__":
+  build_confusion_matrix(sys.argv[1])
