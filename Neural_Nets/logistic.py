726d86b4e406425857bb43932c07bcbc39474e38
daravinds <daravinds@gmail.com>
2018-03-26 23:58:55 -0500

    Complete Neural Nets - and verified for one of k encoding

--- Neural_Nets/logistic.py
+++ Neural_Nets/logistic.py
@@ -3,22 +3,23 @@ import sys
 import pdb
 import math
 import random
+import numpy
 
-class NeuralNet():
+class Logistic():
   learning_rate = 0.0
   epochs = 0
   training_file = None
   test_file = None
   weights = dict()
   weight_differences = dict()
-  # no_of_features = 0
-  # no_of_values = dict()
   metadata = None
   features = list()
   bias = 0.0
   mean_xi = dict()
   std_xi = dict()
   feature_type_map = dict()
+  negative_label = None
+  positive_label = None
 
   def initialize_variables(self):
     self.learning_rate = 0.0
@@ -26,33 +27,31 @@ class NeuralNet():
     self.training_file = None
     self.test_file = None
     self.weights = dict()
-    # self.no_of_features = 0
-    # self.no_of_values = dict()
+    self.weight_differences = dict()
     self.metadata = None
     self.features = list()
-    self.bias = 0.0
+    self.bias = 1.0
     self.mean_xi = dict()
     self.std_xi = dict()
     self.feature_type_map = dict()
+    self.negative_label = None
+    self.positive_label = None
 
   def assign_variables(self, learning_rate, epochs, training_file, test_file):
     self.learning_rate = float(learning_rate)
     self.epochs = int(epochs)
     self.training_file = training_file
     self.test_file = test_file
-    self.features = self.metadata._attrnames[:-1]
+
+    class_label = self.metadata._attrnames[-1]
+    self.negative_label = self.metadata._attributes[class_label][1][0]
+    self.positive_label = self.metadata._attributes[class_label][1][1]
+
+    self.features = list(self.metadata._attrnames[:-1])
 
     feature_details = self.metadata._attributes
     for feature_name in self.features:
       self.feature_type_map[feature_name] = feature_details[feature_name][0]
-      # feature_values = feature_details[feature_name][1]
-
-      # if self.is_real_or_numeric_feature(feature_name):
-      #   self.no_of_values[feature_name] = 1
-      # elif self.is_nominal_feature(feature_name):
-      #   self.no_of_values[feature_name] = len(feature_values)
-
-      # self.no_of_features += self.no_of_values[feature_name]
 
   def is_real_or_numeric_feature(self, feature_name):
     return self.feature_type_map[feature_name] == "numeric" or self.feature_type_map[feature_name] == "real"
@@ -60,32 +59,33 @@ class NeuralNet():
   def is_nominal_feature(self, feature_name):
     return self.feature_type_map[feature_name] == "nominal" or self.feature_type_map[feature_name] == "ordinal"
 
-  def initialize_weights_and_bias(self):
-    self.bias = 1.0
-    self.weights["bias"] = -0.003 # random.uniform(-0.01, 0.01)
+  def initialize_weights(self):
+    # self.weights["bias"] = 0.005
+    self.weights["bias"] = random.uniform(-0.01, 0.01)
     for feature in self.features:
       if self.is_real_or_numeric_feature(feature):
-        self.weights[feature] = -0.003 # random.uniform(-0.01, 0.01)
+        # self.weights[feature] = 0.005
+        self.weights[feature] = random.uniform(-0.01, 0.01)
       elif self.is_nominal_feature(feature):
         self.weights[feature] = dict()
         values = self.metadata._attributes[feature][1]
         for value in values:
           self.weights[feature][value] = random.uniform(-0.01, 0.01)
 
+  def normalize(self, data):
+    for record in data:
+      self.normalize_record(record)
+
   def normalize_record(self, record):
     for feature in self.features:
-      if feature == "bias":
-        continue
       if self.is_real_or_numeric_feature(feature):
         record[feature] = (record[feature] - self.mean_xi[feature]) / self.std_xi[feature]
     return record
 
   def computeZ(self, record):
-    # record = self.normalize_record(record)
     total = self.bias * self.weights["bias"]
     for feature in self.features:
       if self.is_real_or_numeric_feature(feature):
-        # value = (record[feature] - self.mean_xi[feature]) / self.std_xi[feature]
         total += record[feature] * self.weights[feature]
       elif self.is_nominal_feature(feature):
         value = record[feature]
@@ -102,66 +102,28 @@ class NeuralNet():
         self.mean_xi[feature] = (total * 1.0) / len(data)
         sum_std = sum([(record[feature] - self.mean_xi[feature])**2 for record in data])
         self.std_xi[feature] = math.sqrt((sum_std * 1.0) / len(data))
-      # elif self.is_nominal_feature(feature):
-      #   self.mean_xi[feature] = dict()
-      #   self.std_xi[feature] = dict()
-      #   values_count = dict()
-      #   for record in data:
-      #     values_count[record[feature]] = values_count.get(record[feature], 0) + 1
-      #   feature_values = feature_details[feature][1]
-      #   for value in feature_values:
-      #     value_count = values_count.get(value, 0)
-      #     self.mean_xi[feature][value] = (value_count * 1.0) / len(data)
-      #     self.std_xi[feature][value] = math.sqrt((value_count * (1 - self.mean_xi[feature][value])**2 * 1.0) / len(data))
-
 
   def compute_cross_entropy(self, o, y):
-    if o == 1 or o == 0:
-      return 0
-
-    ce = y * math.log(o) + (1 - y) * math.log(1 - o)
-    return -ce
+    ce = -y * math.log(o) - (1 - y) * math.log(1 - o)
+    return ce
 
   def perform_logistic_regression(self, learning_rate, epochs, training_file, test_file):
     self.initialize_variables()
     data, self.metadata = read_data(training_file)
     self.assign_variables(learning_rate, epochs, training_file, test_file)
-    self.initialize_weights_and_bias()
-    # random.shuffle(data)
-    class_label = self.metadata._attrnames[-1]
-    class_labels = self.metadata._attributes[class_label][1]
+    self.initialize_weights()
 
     self.compute_mean_and_standard_deviation(data)
+    self.normalize(data)
     for loop in range(self.epochs):
-      # random.shuffle(data)
-      correct_classifications = 0
-      total_cross_entropy = 0.0
-      for record in data[:1]:
-        pdb.set_trace()
-        record = self.normalize_record(record)
-        # pdb.set_trace()
-        y = record[-1]
-        yInt = class_labels.index(y)
-        z = self.computeZ(record)
-        o = sigmoid(z)
-        if(o <= 0.5):
-          label = 0
-        else:
-          label = 1
-        if label == yInt:
-          # print "o:" + str(o) + " y:" + str(y)
-          correct_classifications += 1
-
-        # pdb.set_trace()
-        error = self.compute_cross_entropy(o, yInt)
-        total_cross_entropy += error
-
+      numpy.random.shuffle(data)
+      for index, record in enumerate(data):
+        o, predicted_label, actual_label = self.predict_instance(record)
         for feature in self.weights.keys():
           weight = self.weights[feature]
           if feature == "bias":
             value = self.bias
           else:
-            # value = (record[feature] - self.mean_xi[feature]) / self.std_xi[feature]
             value = record[feature]
 
           if isinstance(weight, dict):
@@ -169,12 +131,11 @@ class NeuralNet():
             feature_values = self.metadata._attributes[feature][1]
             for val in feature_values:
               if val == value:
-                self.weight_differences[feature][val] = self.learning_rate * (yInt - o)
+                self.weight_differences[feature][val] = self.learning_rate * (actual_label - o)
               else:
                 self.weight_differences[feature][val] = 0
           else:
-            self.weight_differences[feature] = self.learning_rate * (yInt - o) * value
-
+            self.weight_differences[feature] = self.learning_rate * (actual_label - o) * value
         for feature in self.weights.keys():
           weight = self.weights[feature]
           if isinstance(weight, dict):
@@ -183,54 +144,66 @@ class NeuralNet():
               self.weights[feature][val] += self.weight_differences[feature][val]
           else:
             self.weights[feature] += self.weight_differences[feature]
-      print str(loop + 1) + "\t" + str(total_cross_entropy) + "\t" + str(correct_classifications) + "\t" + str(len(data) - correct_classifications)
+
+      correct_classifications = int(0)
+      total_cross_entropy = 0.0
+      for record in data:
+        o, predicted_label, actual_label = self.predict_instance(record)
+        total_cross_entropy += self.compute_cross_entropy(o, actual_label)
+        # print str(o) + " " + str(predicted_label) + " " + str(actual_label)
+
+        if predicted_label == actual_label:
+          correct_classifications += 1
+      print str(loop + 1) + "\t" + str("%0.9f"%total_cross_entropy) + "\t" + str(correct_classifications) + "\t" + str(len(data) - correct_classifications)
+
+
+  def predict_instance(self, normalized_record):
+    total = self.computeZ(normalized_record)
+    o = sigmoid(total)
+
+    if(o <= 0.5):
+      predicted_label = 0
+    else:
+      predicted_label = 1
+
+    if normalized_record[-1] == self.negative_label:
+      actual_label = 0
+    else:
+      actual_label = 1        
+
+    return float(o), int(predicted_label), int(actual_label)
+
 
   def predict(self):
     data, _ = read_data(self.test_file)
-    class_label = self.metadata._attrnames[-1]
-    class_labels = self.metadata._attributes[class_label][1]
-    tp = 0
-    fp = 0
-    tn = 0
-    fn = 0
-    total_positives = 0
+    tp = 0.0
+    fp = 0.0
+    tn = 0.0
+    fn = 0.0
     precision = 0.0
     recall = 0.0
     f1 = 0.0
+    correct_classifications = int(0)
+    self.normalize(data)
     for record in data:
-      record = self.normalize_record(record)
-      total = 0.0
-      for feature in self.weights.keys():
-        if feature == "bias":
-          value = self.bias
-        else:
-          value = record[feature]
-        total += value * self.weights[feature]
-      # pdb.set_trace()
-      o = sigmoid(total)
-      if(o <= 0.5):
-        label = 0
-      else:
-        label = 1
-      predicted_class = class_labels[label]
-      actual_class = record[-1]
-      if actual_class == class_labels[1]:
-        total_positives += 1
-      print str(o) + " " + predicted_class + " " + actual_class
-
-      if predicted_class == actual_class:
-        if predicted_class == class_labels[1]:
-          tp += 1
-        else:
+      o, predicted_label, actual_label = self.predict_instance(record)
+      print str("%0.9f"%o) + "\t" + str(predicted_label) + "\t" + str(actual_label)
+      if predicted_label == actual_label:
+        correct_classifications += 1
+        if predicted_label == 0:
           tn += 1
-      else:
-        if predicted_class == class_labels[1]:
-          fp += 1
         else:
+          tp += 1
+      else:
+        if predicted_label == 0:
           fn += 1
+        else:
+          fp += 1
+
     precision = (1.0 * tp) / (tp + fp)
-    recall = (1.0 * tp) / (total_positives)
-    f1 = 2 * precision * recall / (precision + recall)
+    recall = (1.0 * tp) / (tp + fn)
+    f1 = float((2.0 * precision * recall) / (precision + recall))
+    print str(correct_classifications) + "\t" + str(len(data) - correct_classifications)
     print str(f1)
     return f1
 
@@ -242,16 +215,13 @@ def read_data(file):
   return data, metadata
 
 def classify():
-  neural_net = NeuralNet()
+  logistic = Logistic()
   learning_rate = sys.argv[1]
   epochs = sys.argv[2]
   training_file = sys.argv[3]
   test_file = sys.argv[4]
-  avg_f1 = 0.0
-  f1 = 0.0
-
-  neural_net.perform_logistic_regression(learning_rate, epochs, training_file, test_file)
-  f1 = neural_net.predict()
+  logistic.perform_logistic_regression(learning_rate, epochs, training_file, test_file)
+  f1 = logistic.predict()
 
 if __name__ == "__main__":
 	classify()
---
+++ Neural_Nets/nnet.py
@@ -0,0 +1,286 @@
+from scipy.io import arff
+import sys
+import pdb
+import math
+import random
+import numpy
+
+class NeuralNet():
+  learning_rate = 0.0
+  epochs = 0
+  hidden_units = 0
+  training_file = None
+  test_file = None
+  metadata = None
+  features = list()
+  no_of_features = 0
+  bias = 1.0
+  mean_xi = dict()
+  std_xi = dict()
+  feature_type_map = dict()
+  negative_label = None
+  positive_label = None
+  hidden_outputs = []
+  weightsXtoH = []
+  weightsHtoO = []
+  deltaO = None
+  deltaH = []
+
+  def initialize_variables(self):
+    self.learning_rate = 0.0
+    self.epochs = 0
+    self.hidden_units = 0
+    self.training_file = None
+    self.test_file = None
+    self.metadata = None
+    self.no_of_features = 0
+    self.bias = 1.0
+    self.mean_xi = dict()
+    self.std_xi = dict()
+    self.feature_type_map = dict()
+    self.negative_label = None
+    self.positive_label = None
+    self.hidden_outputs = []
+    self.weightsXtoH = []
+    self.weightsHtoO = []
+    self.deltaO = None
+    self.deltaH = []
+
+  def assign_variables(self, learning_rate, hidden_units, epochs, training_file, test_file):
+    self.learning_rate = float(learning_rate)
+    self.hidden_units = int(hidden_units)
+    for i in range(self.hidden_units):
+      self.hidden_outputs.append(0)
+      self.deltaH.append(0)
+    self.epochs = int(epochs)
+    self.training_file = training_file
+    self.test_file = test_file
+
+    class_label = self.metadata._attrnames[-1]
+    self.negative_label = self.metadata._attributes[class_label][1][0]
+    self.positive_label = self.metadata._attributes[class_label][1][1]
+
+    feature_names = self.metadata._attrnames[:-1]
+    feature_details = self.metadata._attributes
+    self.no_of_features = 0
+    
+
+    for feature_name in feature_names:
+      self.feature_type_map[feature_name] = feature_details[feature_name][0]
+      if self.is_real_or_numeric_feature(feature_name):
+        self.no_of_features += 1
+      else:
+        self.no_of_features += len(feature_details[feature_name][1])
+
+
+  def is_real_or_numeric_feature(self, feature_name):
+    return self.feature_type_map[feature_name] == "numeric" or self.feature_type_map[feature_name] == "real"
+
+  def is_nominal_feature(self, feature_name):
+    return self.feature_type_map[feature_name] == "nominal" or self.feature_type_map[feature_name] == "ordinal"
+
+  def initialize_weights(self):
+    for i in range(self.no_of_features + 1):
+      self.weightsXtoH.append([])
+      for j in range(self.hidden_units):
+        # self.weightsXtoH[i].append(0.005)
+        self.weightsXtoH[i].append(random.uniform(-0.01, 0.01))
+
+    for j in range(self.hidden_units + 1):
+      # self.weightsHtoO.append(0.005)
+      self.weightsHtoO.append(random.uniform(-0.01, 0.01))
+
+  def compute_cross_entropy(self, o, y):
+    ce = -y * math.log(o) - (1 - y) * math.log(1 - o)
+    return ce
+
+  def generate_one_of_k_vector(self, all_values, val):
+    one_of_k = []
+    for value in all_values:
+      if value == val:
+        one_of_k.append(1)
+      else:
+        one_of_k.append(0)
+    return one_of_k
+
+  def compute_mean_and_standard_deviation(self, data):
+    feature_names = self.metadata._attrnames[:-1]
+    feature_details = self.metadata._attributes
+
+    for feature in feature_names:
+      if self.is_real_or_numeric_feature(feature):
+        total = sum([record[feature] for record in data])
+        self.mean_xi[feature] = (total * 1.0) / len(data)
+        sum_std = sum([(record[feature] - self.mean_xi[feature])**2 for record in data])
+        self.std_xi[feature] = math.sqrt((sum_std * 1.0) / len(data))
+
+
+  def normalize(self, data):
+    feature_names = self.metadata._attrnames[:-1]
+    feature_details = self.metadata._attributes
+
+    for feature in feature_names:
+      if self.is_real_or_numeric_feature(feature):
+        for record in data:
+          record[feature] = (record[feature] - self.mean_xi[feature]) / self.std_xi[feature]
+
+    normalized_data = []
+    for record in data:
+      row = []
+      row.append(self.bias)
+      for feature in feature_names:
+        if self.is_real_or_numeric_feature(feature):
+          row.append(record[feature])
+        else:
+          values = feature_details[feature][1]
+          row.extend(self.generate_one_of_k_vector(values, record[feature]))
+      row.append(record[-1])
+      normalized_data.append(row)
+
+    return normalized_data
+
+  def perform_forward_propagation(self, record):
+    for j in range(self.hidden_units):
+      z = 0.0
+      for i in range(self.no_of_features + 1):
+        # print str(self.weightsXtoH[i][j]) + "  " + str(record[i])
+        # pdb.set_trace()
+        z += (self.weightsXtoH[i][j] * record[i])
+      # print "Hidden input (weighted sum):" + str(z)
+      self.hidden_outputs[j] = sigmoid(z)
+      # print "Hidden output: " + str(self.hidden_outputs[j])
+      # print str(j) + " " + str(self.hidden_outputs[j])
+
+    zOutput = self.bias * self.weightsHtoO[0]
+    # print "zOutput: " + str(zOutput)
+    for j in range(self.hidden_units):
+      # print str(self.weightsHtoO[j + 1]) + "  " + str(self.hidden_outputs[j])
+      zOutput += (self.weightsHtoO[j + 1] * self.hidden_outputs[j])
+    # print "zOutput: " + str(zOutput)
+
+    o = sigmoid(zOutput)
+    # print "o: " + str(o)
+    if(o <= 0.5):
+      predicted_label = 0
+    else:
+      predicted_label = 1
+    # print "Predicted:" + str(predicted_label)
+    if record[-1] == self.negative_label:
+      actual_label = 0
+    else:
+      actual_label = 1        
+
+    return o, int(predicted_label), int(actual_label)
+
+  def compute_errors(self, o, y):
+    self.deltaO = float(y - o)
+    # print "deltaO:" + str(self.deltaO)
+    for j in range(self.hidden_units):
+      delta = self.hidden_outputs[j] * (1 - self.hidden_outputs[j]) * self.deltaO * self.weightsHtoO[j + 1]
+      # print "deltaH[" + str(j) + "]: " + str(delta)
+      self.deltaH[j] = delta
+
+  def print2D(self, a):
+    for row in a:
+      print row
+
+  def compute_gradients(self, record):
+    # print "Before WeightHtoO: " + str(self.weightsHtoO[0])
+    # print "BiasDiff HtoO: " + str(self.learning_rate * self.deltaO)
+    self.weightsHtoO[0] += (self.learning_rate * self.deltaO)
+    # print "After WeightHtoO: " + str(self.weightsHtoO[0])
+    for j in range(self.hidden_units):
+      # print "Before WeightHtoO: " + str(self.weightsHtoO[j + 1])
+      # print "DiffHtoO: " + str(self.learning_rate * self.deltaO * self.hidden_outputs[j])
+      self.weightsHtoO[j + 1] += (self.learning_rate * self.deltaO * self.hidden_outputs[j])
+      # print "After WeightHtoO: " + str(self.weightsHtoO[j + 1])
+
+    for j in range(self.hidden_units):
+      for i in range(self.no_of_features + 1):
+        # print "Before WeightXtoH: " + str(self.weightsXtoH[i][j])
+        self.weightsXtoH[i][j] += (self.learning_rate * self.deltaH[j] * record[i])
+        # print "After WeightXtoH: " + str(self.weightsXtoH[i][j])
+
+
+  def perform_training(self, learning_rate, hidden_units, epochs, training_file, test_file):
+    self.initialize_variables()
+    data, self.metadata = read_data(training_file)
+    self.assign_variables(learning_rate, hidden_units, epochs, training_file, test_file)
+    self.compute_mean_and_standard_deviation(data)
+    normalized_data = self.normalize(data)
+    # self.print2D(normalized_data)
+    self.initialize_weights()
+
+    for loop in range(self.epochs):
+      numpy.random.shuffle(normalized_data)
+      # self.print2D(self.weightsXtoH)
+
+      for index, record in enumerate(normalized_data):
+        o, predicted_label, actual_label = self.perform_forward_propagation(record)
+        # print str(o)
+        self.compute_errors(o, actual_label)
+        self.compute_gradients(record)
+
+      correct_classifications = 0
+      total_cross_entropy = 0.0
+      for record in normalized_data:
+        o, predicted_label, actual_label = self.perform_forward_propagation(record)
+        total_cross_entropy += self.compute_cross_entropy(o, actual_label)
+        # print str(o) + " " + str(predicted_label) + " " + str(actual_label)
+        if predicted_label == actual_label:
+          correct_classifications += 1
+      print str(loop + 1) + "\t" + str("%0.9f"%total_cross_entropy) + "\t" + str(correct_classifications) + "\t" + str(len(data) - correct_classifications)
+
+  def predict(self):
+    data, _ = read_data(self.test_file)
+    tp = 0.0
+    fp = 0.0
+    tn = 0.0
+    fn = 0.0
+    precision = 0.0
+    recall = 0.0
+    f1 = 0.0
+    correct_classifications = int(0)
+    normalized_data = self.normalize(data)
+    for record in normalized_data:
+      o, predicted_label, actual_label = self.perform_forward_propagation(record)
+      print str("%0.9f"%o) + "\t" + str(predicted_label) + "\t" + str(actual_label)
+      if predicted_label == actual_label:
+        correct_classifications += 1
+        if predicted_label == 0:
+          tn += 1
+        else:
+          tp += 1
+      else:
+        if predicted_label == 0:
+          fn += 1
+        else:
+          fp += 1
+
+    precision = (1.0 * tp) / (tp + fp)
+    recall = (1.0 * tp) / (tp + fn)
+    f1 = float((2.0 * precision * recall) / (precision + recall))
+    print str(correct_classifications) + "\t" + str(len(data) - correct_classifications)
+    print str(f1)
+    return f1
+
+def sigmoid(value):
+  return 1.0 / (1.0 + math.exp(-value))
+
+def read_data(file):
+  data, metadata = arff.loadarff(open(file, "r"))
+  return data, metadata
+
+def classify():
+  neural_net = NeuralNet()
+  learning_rate = sys.argv[1]
+  hidden_units = sys.argv[2]
+  epochs = sys.argv[3]
+  training_file = sys.argv[4]
+  test_file = sys.argv[5]
+  neural_net.perform_training(learning_rate, hidden_units, epochs, training_file, test_file)
+  # pdb.set_trace()
+  f1 = neural_net.predict()
+
+if __name__ == "__main__":
+	classify()
